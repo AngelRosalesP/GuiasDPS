3d406f70572564c7d61362b4ae88481f304caa73
AngelRosalesP <angel.rosalesp24@gmail.com>
2022-10-03 17:41:49 -0600

    Guias de DPS
    3,4,5

---
+++ guia4/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as guia4} from './app.json';
+
+AppRegistry.registerComponent(guia4, () => App);
